dce49bca4693ff98301729e9e6f9254a64411b36
Leonel Moyano <leonelmoyano1809@gmail.com>
2020-11-09 20:40:43 -0300

    monohilo JS

---
+++ conceptos/monohilo.js
@@ -0,0 +1,15 @@
+console.log("Que onda!");
+
+let i = 0;
+setInterval(function () {
+    console.log(i++);
+    try {
+        if (i === 5) {
+            var a = 3 + z;
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}, 1000)
+
+console.log("jojo");
